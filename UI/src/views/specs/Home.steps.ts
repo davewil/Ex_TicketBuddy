7dbabed9cf9858352a64504bf6b6de6c16b05cba
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-08-12 15:46:31 +0100

    fix frontend

--- UI/src/views/specs/Home.steps.ts
+++ UI/src/views/specs/Home.steps.ts
@@ -1,16 +1,18 @@
 ﻿import {MockServer} from "../../testing/mock-server.ts";
 import {afterEach, beforeEach} from "vitest";
-import {Events} from "../../testing/data.ts";
+import {Events, Users} from "../../testing/data.ts";
 import {eventExists, renderHome, unmountHome} from "./Home.page.tsx";
 import {waitUntil} from "../../testing/utilities.ts";
 import {expect} from "vitest";
 
 const mockServer = MockServer.New();
-let wait_for_get: () => boolean;
+let wait_for_get_events: () => boolean;
+let wait_for_get_users: () => boolean;
 
 beforeEach(() => {
     mockServer.reset();
-    wait_for_get = mockServer.get("events", Events)
+    wait_for_get_events = mockServer.get("events", Events)
+    wait_for_get_users = mockServer.get("users", Users);
     mockServer.start();
 });
 
@@ -20,7 +22,8 @@ afterEach(() => {
 
 export async function should_load_events_on_render() {
     renderHome();
-    await waitUntil(wait_for_get);
+    await waitUntil(wait_for_get_events);
+    await waitUntil(wait_for_get_users);
     for (const event of Events) {
         expect(eventExists(event.EventName)).toBeTruthy();
     }
