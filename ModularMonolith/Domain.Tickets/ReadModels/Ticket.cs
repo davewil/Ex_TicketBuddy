ebe8167f9244dfed76529714206ee630af8b46bf
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-09-03 16:00:26 +0100

    refactor

--- ModularMonolith/Domain.Tickets/ReadModels/Ticket.cs
+++ ModularMonolith/Domain.Tickets/ReadModels/Ticket.cs
@@ -3,8 +3,8 @@
 public class Ticket(Guid Id, Guid eventId, decimal price, uint SeatNumber, bool Purchased)
 {
     public Guid Id { get; } = Id;
-    public Guid EventId { get; private set; } = eventId;
-    public decimal Price { get; private set; } = price;
-    public uint SeatNumber { get; private set; } = SeatNumber;
-    public bool Purchased { get; private set; } = Purchased;
+    public Guid EventId { get; } = eventId;
+    public decimal Price { get; } = price;
+    public uint SeatNumber { get; } = SeatNumber;
+    public bool Purchased { get; } = Purchased;
 }
